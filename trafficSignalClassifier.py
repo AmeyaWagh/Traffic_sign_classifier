bb0dd57bf640bcad7eaa803a716d822bcfab1c37
AmeyaWagh <ameya555@gmail.com>
2017-10-27 09:33:11 -0400

    tested prediction with external image

--- trafficSignalClassifier.py
+++ trafficSignalClassifier.py
@@ -10,18 +10,19 @@ from keras.utils import np_utils
 
 class Classifier:
 
-    def __init__(self, imageShape=(28, 28, 1),nClasses=5):
+    def __init__(self, imageShape=(28, 28, 1), nClasses=5):
         self.modelPath = './model'
         self.signNamesFile = './signnames.csv'
         self.validation_file = './data/validate.p'
         self.imageShape = imageShape
-        self.nClasses=nClasses
+        self.nClasses = nClasses
         self.signNames = self.readSignNameCSV()
         self.model = self.loadModel()
 
     def readSignNameCSV(self, nClasses=None):
         with open(self.signNamesFile, 'r') as fp:
             data = list(csv.DictReader(fp))
+
         # convert dictionary to list and format file names
         data = [
             everyData['SignName'] for everyData in data]
@@ -30,51 +31,77 @@ class Classifier:
             data = data[0:self.nClasses]
         return data
 
-    def Gray_image(self,image):
+    def convertToGray(self, image):
+        '''
+            convert image to gray scale
+        '''
         return np.resize(cv2.cvtColor(
             image, cv2.COLOR_RGB2YCrCb)[:, :, 0], self.imageShape)
 
-    def normalize_image(self,image):
-        return  (image*1.0)/(255.0) - 0.5
-    
+    def normalizeImage(self, image):
+        '''
+            Normalize image to with zero mean
+        '''
+        return (image*1.0)/(255.0) - 0.5
+
     def preProcessImage(self, image):
         img = []
         for i in image:
-            img.append(self.normalize_image(self.Gray_image(i)))
+            img.append(self.normalizeImage(self.convertToGray(i)))
         img = np.array(img)
         return img
 
     def loadModel(self):
-        # load json and create model
-        # json_file = open(os.path.join(modelPath,'model.json'), 'r')
-        # loaded_model_json = json_file.read()
-        # json_file.close()
-        with open(os.path.join(self.modelPath,'model.json'), 'r') as json_file:
+        '''
+            load trained model from model.json and weights from model.h5
+        '''
+        with open(
+                os.path.join(self.modelPath, 'model.json'), 'r') as json_file:
             loaded_model_json = json_file.read()
 
         loaded_model = model_from_json(loaded_model_json)
+
         # load weights into new model
-        loaded_model.load_weights(os.path.join(self.modelPath,"model.h5"))
+        loaded_model.load_weights(os.path.join(self.modelPath, "model.h5"))
         loaded_model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+                             optimizer='adam',
+                             metrics=['accuracy'])
         print("Loaded model from disk")
         return loaded_model
 
-    def predict(self,X):
+    def predict(self, X):
         X = self.preProcessImage(X)
+        print np.shape(X)
         predictions = self.model.predict(X)
-        predictions = [pred.index(max(pred)) for pred in predictions.tolist()]
+        predictions = [self.signNames[pred.index(
+            max(pred))] for pred in predictions.tolist()]
         return predictions
 
 if __name__ == '__main__':
-    classifier = Classifier(imageShape=(28, 28, 1),nClasses=5)
-    
-    validation_file= './data/validate.p'
+    classifier = Classifier(imageShape=(28, 28, 1), nClasses=5)
+
+    validation_file = './data/validate.p'
 
     with open(validation_file, mode='rb') as f:
         valid = pickle.load(f)
 
-    X_valid, y_valid = valid['features'], valid['labels']
-    
-    print classifier.predict(X_valid)
+
+    #-------------------------------------------------------#
+    _im = cv2.imread('./image.jpg')
+    _im = np.array(_im)    
+    #-------------------------------------------------------#    
+
+    # X_valid, y_valid = valid['features'], valid['labels']
+    X_valid = [_im]
+
+    try:
+        for img in X_valid:
+            print classifier.predict([img])
+            # img=np.resize(img,(100,100,1))
+            cv2.imshow('image', img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+    except:
+        cv2.destroyAllWindows()
+        print "bye\n\n"
+        quit()
